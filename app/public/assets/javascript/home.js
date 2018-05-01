dacada6440bae988c6eccf1fab2fea95a59bcafd
The Mai <Mai.The.09@gmail.com>
2018-04-30 23:42:25 -0500

    add animations to home.html

--- app/public/assets/javascript/home.js
+++ app/public/assets/javascript/home.js
@@ -1,5 +1,28 @@
 $(document).ready(function () {
-    $('.title-button').on('click', function () {
-        console.log("hi");
+    $('#white-block').animate({
+        // width: '+100vh',
+        height: '310px'
+    }, 500, 'easeOutCubic', function () {
+        $('#white-block').animate({
+            width: '310px'
+        }, 500, 'easeOutCubic', function () {
+            $('#title-container').animate({
+                opacity: 1
+            }, 500, 'easeInOutQuad');
+        });
+    });
+    $('.title-button').on('click', function () {        
+        $('#white-block').animate({
+            height: '100vh'
+        }, 500, 'easeOutCubic', function () {
+            $('#white-block').animate({
+                width: '100vw'
+            }, 500, 'easeOutCubic');
+        });
+        $('#title-container').animate({
+            top: '-150px'
+        }, 1000, 'easeInBack', function () {
+            // window.location.href = "/survey";
+        });
     })
 });
